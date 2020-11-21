d8e66461028a1b2b672a24130381c6106a8fe313
TheGreatestPretender <katschmidt201@gmail.com>
2020-11-21 11:33:00 -0500

    finished original commands

--- commands/guildProg.js
+++ commands/guildProg.js
@@ -5,41 +5,22 @@ module.exports = {
     description: 'Displays the Rock Game Shops raid progression (current expansion only)',
     args: true,
     execute(message, args){
-        let difficulty = args[0];
+        let raid = args[0];
         
-        let raids = [];
-        
-        let regexPattern = /[0-9]\/[0-9]/;
+        const prettyRaid = raid.replace(/\-/g, ' ');
 
-        let progressionObj = {
-            raid: '',
-            difficulty: '',
-            progression: '',
-        }
+        let raidObj = {
+            raid: prettyRaid,
+            summary: ''
+        };
+
+
         fetch('https://raider.io/api/v1/guilds/profile?region=us&realm=thunderlord&name=The%20Rock%20Game%20Shop&fields=raid_progression%2C%20raid_rankings')
         .then(res => res.json())
         .then(data => {
-
-            for (let i = 0; i < data.length; i++) {
-                progressionObj.raid = Object.keys(data.raid_progression[i]);
-            }
+            raidObj.summary = data.raid_progression[raid].summary;
             
-            console.dir(progressionObj);
-            /**let objLength = Object.keys(data.raid_progression).length;
-            
-            for (let prop in data.raid_progression) {
-                let guildRaidObj = new Object();
-                guildRaidObj.raid = Object.getOwnPropertyNames(data.raid_progression);
-                //console.dir(data.raid_progression)
-                raids.push(guildRaidObj);
-            }
-            console.dir(raids);
-            //raidArr.push(Object.keys(data.raid_progression))
-
-          /*   for (let prop in data.raid_progression) {
-                console.dir(data.raid_progression[prop].summary)
-            } */
-
+            message.channel.send(`The Rock Game Shop progress on ${raidObj.raid} is ${raidObj.summary}`);
         })
         .catch (err => console.error(err));
     }
