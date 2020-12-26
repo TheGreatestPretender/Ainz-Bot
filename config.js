384610f1fd011874ff02e8526082e1c782eb4129
TheGreatestPretender <katschmidt201@gmail.com>
2020-12-26 11:21:37 -0500

    more config changes

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let aws = require('aws-sdk');
 
 let config = {
     "prefix" : "!",
-    "token" : "NzY3NDI3ODc1MzQ2ODQxNjQy.X4xw5w.fSovoH7hkb7tTjtsqyIA4YzjYrY"
+    "token" : "NzY3NDI3ODc1MzQ2ODQxNjQy.X4xw5w.8qCA_pC1enpS00jm1qMFXaZtXYQ"
 }
 
 module.exports = config;
