01ea36e63bac62524a2289dce6cb2e2efb261530
TheGreatestPretender <katschmidt201@gmail.com>
2020-12-26 11:20:19 -0500

    why i keep messing up?

--- config.js
+++ config.js
@@ -3,14 +3,6 @@ let aws = require('aws-sdk');
 
 
 let config = {
-    "awsConfigs": {
-        "aws_table_name": 'XyronReviews',
-        "aws_remote_config": {
-            accessKeyId: 'AKIA45SC6DPNOYWZIG4Z',
-            secretAccessKeyId: 'wZRsF+4QxsIyTwhG8cVowX1znmz5KAsBE5i1zwoi',
-            region: 'us-east-1'
-        }
-    },
     "prefix" : "!",
     "token" : "NzY3NDI3ODc1MzQ2ODQxNjQy.X4xw5w.fSovoH7hkb7tTjtsqyIA4YzjYrY"
 }
