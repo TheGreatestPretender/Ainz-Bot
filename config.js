bf1c3e89ff4fda22ca3cbbced8bc639b505678bb
Kat Schmidt <katschmidt201@gmail.com>
2020-12-26 11:23:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let aws = require('aws-sdk');
 
 let config = {
     "prefix" : "!",
-    "token" : "NzY3NDI3ODc1MzQ2ODQxNjQy.X4xw5w.8qCA_pC1enpS00jm1qMFXaZtXYQ"
+    "token" : "token"
 }
 
 module.exports = config;
