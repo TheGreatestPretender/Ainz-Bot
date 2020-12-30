9980c163491b34671537570c0f14aa9aa48a26e9
Kat Schmidt <katschmidt201@gmail.com>
2020-12-30 13:57:27 -0500

    Updated help to add two commands: add-review and xyron-rating

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,7 @@ module.exports = {
   name: 'help',
   description: 'Shows all the options',
   execute(message){
-      message.channel.send('**!affix <name of current season m+ affix>** : Gives you the word-for-word Blizzard text for said affix\n**!mplus <character name> <server name>** : Get leader board stats for m+ on your character/server combination\n**!affixes **: Gets the weekly affixes for NA\n**!progression <name of current expansion raid>** : Shows you the progress on the highest difficulty that the guild has completed (whole guild does not need to be present to have this be recorded. Just needs to be a \'guild run \').\n\n If you have any new command ideas ask Foxx :heart:');
+      message.channel.send('**!affix <name of current season m+ affix>** : Gives you the word-for-word Blizzard text for said affix\n**!mplus <character name> <server name>** : Get leader board stats for m+ on your character/server combination\n**!affixes **: Gets the weekly affixes for NA\n**!progression <name of current expansion raid>** : Shows you the progress on the highest difficulty that the guild has completed (whole guild does not need to be present to have this be recorded. Just needs to be a \'guild run \').\n**!add-review <the review you want to submit>**: Add a review (specifically on Xyron being... Xyron)\n**!xyron-rating**: Get a random review on Xyron because we love him\n\n If you have any new command ideas ask Foxx :heart:');
   }
 };
+                           
