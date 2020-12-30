4bd8928907708138708f8b1ae23320a47b206e19
Kat Schmidt <katschmidt201@gmail.com>
2020-12-30 13:37:04 -0500

    Finished get-review

--- commands/get-review.js
+++ commands/get-review.js
@@ -1,42 +1,50 @@
 const AWS = require('aws-sdk');
 
-let awsConfig = {
-    "region": "region",
-    "endpoint": "end point",
-    "accessKeyId": "access key",
-    "secretAccessKey": "secret access key"
-};
+//basic AWS Dynamo Configuration
+const awsConfig = {
+    'region': 'your region',
+    'endpoint': 'your endpoint',
+    'accessKeyId': 'your accessKeyId',
+    'secretAccessKey': 'your accesskey'
+}
+
 AWS.config.update(awsConfig);
 
 let docClient = new AWS.DynamoDB.DocumentClient();
-
-const randoNumbero = () => {
-    return Math.floor(Math.random() * 10) + 1;
-}
 
 module.exports = {
     name: 'xyron-rating',
     description: 'Get a random Xyron Yelp review and rate it randomly',
     execute(message) {
-
-
-
+        let listOfReviews = [];
         let params = {
             TableName: 'XyronReviews',
-            Key: 'reviews',
-            Item: {
-                "user": message.author.username
-            },
         };
         
-        docClient.get(params, (err, data) => {
+        //function that triggers when command is called
+        const onScan = (err, data) => {
             if (err) {
-                console.log('womp - ' + JSON.stringify(err, null, 2) );
-                message.channel.send('more womp');
+                console.error('Unable to get review - ' + JSON.stringify(err, null, 2));
+                message.channel.send('Unable to get review');
             } else {
-                console.log('yay');
-                message.channel.send('yay!');
+                console.log('Scan success!');
+                //loop through the returned query and add only the 'reviews' value into list of reviews
+                data.Items.forEach((item) => {
+                    listOfReviews.push(item.reviews);
+                });
+                
+                if (typeof data.LastEvaluatedKey != 'undefined') {
+                    console.log('Scanning for more...');
+                    params.ExclusiveStartKey = data.LastEvaluatedKey;
+                    docClient.scan(params, onScan);
+                };
+                console.log(listOfReviews[Math.floor(Math.random() * listOfReviews.length)]);
+                //return a randomized review
+                return message.channel.send(listOfReviews[Math.floor(Math.random() * listOfReviews.length)]);
             }
-        })
+        };
+
+        console.log('scanning reviews table');
+        docClient.scan(params, onScan);
     }
 }
