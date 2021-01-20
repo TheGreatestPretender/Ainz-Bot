fa1d6c978f0892b1d94f755d22093cb9333073eb
TheGreatestPretender <katschmidt201@gmail.com>
2021-01-20 14:43:02 -0500

    added makeDateReadable and randU32Sync

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const convertArrayToObj = (array, key) => {
     const initVal = {};
     return array.reduce((obj, item) => {
@@ -8,6 +10,28 @@ const convertArrayToObj = (array, key) => {
     }, initVal);
 };
 
+//instead of ugly ISO date => month/day/year
+const makeDateReadable = (inputString) => {
+    //creating regex for proper date format
+    const regEx = /(\d{4})\-(\d{2})\-(\d{2}).*/;
+
+    //returns 0 or 1
+    const boolFullDate = regEx.test(inputString);
+
+    if (boolFullDate === true) {
+        const match = inputString.match(regEx);
+        return `${match[2]}/${match[3]}/${match[1]}`;
+    } else null;
+}
+
+//easy way to generator 32 bit ints
+const randU32Sync = () => {
+  return crypto.randomBytes(4).readUInt32BE(0, true);
+}
+
+
 module.exports = {
-    convertArrayToObj: convertArrayToObj
+    convertArrayToObj: convertArrayToObj,
+    makeDateReadable: makeDateReadable,
+    randU32Sync: randU32Sync
 }
